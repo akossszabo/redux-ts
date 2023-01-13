7fda0207fae45a8b4c2e2b4f25594f66251a84dd
szaboa <akos.szabo@jet-sol.hu>
2023-01-13 16:31:47 +0100

    project init

---
+++ src/hooks/useTypedSelector.ts
@@ -0,0 +1,4 @@
+import { useSelector, TypedUseSelectorHook } from 'react-redux';
+import { RootState } from '../state';
+
+export const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector; //This way useSelector can be used with typescript
